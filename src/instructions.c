636924fbd8613e9a3f0640fa0ab07c808db82ade
Rixhee <rishitbansals@gmail.com>
2025-04-15 14:24:37 -0400

    Added documentation

--- src/instructions.c
+++ src/instructions.c
@@ -2,6 +2,14 @@
 
 #include "utils.c"
 
+/**
+ * Extends a signed number with 'bit_count' bits to a 16-bit signed integer.
+ *
+ * @param x: The value to be sign-extended.
+ * @param bit_count: The number of bits in the original value.
+ *
+ * @return The 16-bit sign-extended value.
+ */
 uint16_t sign_extend(uint16_t x, int bit_count) {
   if ((x >> (bit_count - 1)) & 1) {
     x |= (0xFFFF << bit_count);
@@ -9,6 +17,19 @@ uint16_t sign_extend(uint16_t x, int bit_count) {
   return x;
 }
 
+/**
+ * Implements the ADD instruction.
+ * Performs addition with either two registers or a register and an immediate
+ * value.
+ *
+ * Format:
+ * - 0001 DR SR1 0 00 SR2 (Register mode)
+ * - 0001 DR SR1 1 imm5   (Immediate mode)
+ *
+ * @param instr:  Pointer to the instruction.
+ *
+ * @return void
+ */
 void add_instr(uint16_t* instr) {
   uint16_t dest_reg = (*instr >> 9) & 0x7;
   uint16_t first_value_reg = (*instr >> 6) & 0x7;
@@ -27,6 +48,17 @@ void add_instr(uint16_t* instr) {
   update_flags(dest_reg);
 }
 
+/**
+ * Implements the LDI (Load Indirect) instruction.
+ * Loads a value from the address found at a PC-relative offset.
+ *
+ * Format:
+ * - 1010 DR PCoffset9
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void ldi_instr(uint16_t* instr) {
   uint16_t dest_reg = (*instr >> 9) & 0x7;
   uint16_t pc_offset = sign_extend(*instr & 0x1FF, 9);
@@ -37,6 +69,18 @@ void ldi_instr(uint16_t* instr) {
   update_flags(dest_reg);
 }
 
+/**
+ * Implements the AND instruction.
+ * Performs bitwise AND between two registers or with an immediate value.
+ *
+ * Format:
+ * - 0101 DR SR1 0 00 SR2
+ * - 0101 DR SR1 1 imm5
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void and_instr(uint16_t* instr) {
   uint16_t dest_reg = (*instr >> 9) & 0x7;
   uint16_t first_value_reg = (*instr >> 6) & 0x7;
@@ -53,6 +97,17 @@ void and_instr(uint16_t* instr) {
   update_flags(dest_reg);
 }
 
+/**
+ * Implements the NOT instruction.
+ * Performs bitwise NOT on a register and stores the result.
+ *
+ * Format:
+ * - 1001 DR SR 111111
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void not_instr(uint16_t* instr) {
   uint16_t dest_reg = (*instr >> 9) & 0x7;
   uint16_t value_reg = (*instr >> 6) & 0x7;
@@ -61,6 +116,17 @@ void not_instr(uint16_t* instr) {
   update_flags(dest_reg);
 }
 
+/**
+ * Implements the BR (Branch) instruction.
+ * Updates PC if condition flags match.
+ *
+ * Format:
+ * - 0000 n z p PCoffset9
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void branch_instr(uint16_t* instr) {
   uint16_t pc_offset = sign_extend(*instr & 0x1FF, 9);
   uint16_t cond_flag = (*instr >> 9) & 0x7;
@@ -70,12 +136,35 @@ void branch_instr(uint16_t* instr) {
   }
 }
 
+/**
+ * Implements the JMP or RET instruction.
+ * Sets PC to the value in a register.
+ *
+ * Format:
+ * - 1100 000 BaseR 000000
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void jump_instr(uint16_t* instr) {
   /* Also handles RET */
   uint16_t r1 = (*instr >> 6) & 0x7;
   reg[R_PC] = reg[r1];
 }
 
+/**
+ * Implements the JSR and JSRR instructions.
+ * Jumps to a subroutine and stores return address in R7.
+ *
+ * Format:
+ * - 0100 1 PCoffset11    (JSR)
+ * - 0100 0 00 BaseR ...  (JSRR)
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void jump_register_instr(uint16_t* instr) {
   uint16_t long_flag = (*instr >> 11) & 1;
   reg[R_R7] = reg[R_PC];
@@ -90,6 +179,17 @@ void jump_register_instr(uint16_t* instr) {
   }
 }
 
+/**
+ * Implements the LD (Load) instruction.
+ * Loads a value from a PC-relative memory location into a register.
+ *
+ * Format:
+ * - 0010 DR PCoffset9
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void load_instr(uint16_t* instr) {
   uint16_t dest_reg = (*instr >> 9) & 0x7;
   uint16_t pc_offset = sign_extend(*instr & 0x1FF, 9);
@@ -98,6 +198,17 @@ void load_instr(uint16_t* instr) {
   update_flags(dest_reg);
 }
 
+/**
+ * Implements the LDR (Load Register) instruction.
+ * Loads a value using base register + signed 6-bit offset.
+ *
+ * Format:
+ * - 0110 DR BaseR offset6
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void load_reg_instr(uint16_t* instr) {
   uint16_t dest_reg = (*instr >> 9) & 0x7;
   uint16_t value_reg = (*instr >> 6) & 0x7;
@@ -107,6 +218,17 @@ void load_reg_instr(uint16_t* instr) {
   update_flags(dest_reg);
 }
 
+/**
+ * Implements the LEA (Load Effective Address) instruction.
+ * Loads the address computed from PC + offset into a register.
+ *
+ * Format:
+ * - 1110 DR PCoffset9
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void load_eff_addr_instr(uint16_t* instr) {
   uint16_t dest_reg = (*instr >> 9) & 0x7;
   uint16_t pc_offset = sign_extend(*instr & 0x1FF, 9);
@@ -114,18 +236,51 @@ void load_eff_addr_instr(uint16_t* instr) {
   update_flags(dest_reg);
 }
 
+/**
+ * Implements the ST (Store) instruction.
+ * Stores a register value into a PC-relative memory address.
+ *
+ * Format:
+ * - 0011 SR PCoffset9
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void store_instr(uint16_t* instr) {
   uint16_t dest_reg = (*instr >> 9) & 0x7;
   uint16_t pc_offset = sign_extend(*instr & 0x1FF, 9);
   mem_write(reg[R_PC] + pc_offset, reg[dest_reg]);
 }
 
+/**
+ * Implements the STI (Store Indirect) instruction.
+ * Stores a value into the memory location found via a PC-relative pointer.
+ *
+ * Format:
+ * - 1011 SR PCoffset9
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void store_indirect_instr(uint16_t* instr) {
   uint16_t dest_reg = (*instr >> 9) & 0x7;
   uint16_t pc_offset = sign_extend(*instr & 0x1FF, 9);
   mem_write(mem_read(reg[R_PC] + pc_offset), reg[dest_reg]);
 }
 
+/**
+ * Implements the STR (Store Register) instruction.
+ * Stores a register value into memory using base register + offset.
+ *
+ * Format:
+ * - 0111 SR BaseR offset6
+ *
+ * @param instr: Pointer to the instruction.
+ *
+ * @return void
+ */
 void store_reg_instr(uint16_t* instr) {
   uint16_t dest_reg = (*instr >> 9) & 0x7;
   uint16_t offset_reg = (*instr >> 6) & 0x7;
