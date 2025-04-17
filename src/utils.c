dec8f8a05580f71305f47e6da822fff6d215009a
Rixhee <rishitbansals@gmail.com>
2025-04-17 02:24:13 -0400

    Added utils.h
    Wrote a header file for all the enums and shared func(update_flags)
    definition and added a c file for update_flags implementation

--- src/utils.c
+++ src/utils.c
@@ -1,65 +1,9 @@
-#include <stdint.h>
+#include "utils.h"
 
-// Registers Enum
-enum {
-  R_R0 = 0,
-  R_R1,
-  R_R2,
-  R_R3,
-  R_R4,
-  R_R5,
-  R_R6,
-  R_R7,
-  R_PC, /* Program Counter */
-  R_COND,
-  R_COUNT
-};
+#include <stdint.h>
 
 // Register Storage
 uint16_t reg[R_COUNT];
-
-// Opcodes for Instructions
-enum {
-  OP_BR = 0, /* branch */
-  OP_ADD,    /* add */
-  OP_LD,     /* load */
-  OP_ST,     /* store */
-  OP_JSR,    /* jump register */
-  OP_AND,    /* bitwise and */
-  OP_LDR,    /* load register */
-  OP_STR,    /* store register */
-  OP_RTI,    /* unused */
-  OP_NOT,    /* bitwise not */
-  OP_LDI,    /* load indirect */
-  OP_STI,    /* store indirect */
-  OP_JMP,    /* jump */
-  OP_RES,    /* reserved (unused) */
-  OP_LEA,    /* load effective address */
-  OP_TRAP    /* execute trap */
-};
-
-enum {
-  FL_POS = 1 << 0, /* Positive */
-  FL_ZRO = 1 << 1, /* Zero */
-  FL_NEG = 1 << 2  /* Negative */
-};
-
-enum {
-  TRAP_GETC =
-      0x20, /* get character from keyboard, not echoed onto the terminal */
-  TRAP_OUT = 0x21,   /* output a character */
-  TRAP_PUTS = 0x22,  /* output a word string */
-  TRAP_IN = 0x23,    /* get character from keyboard, echoed onto the terminal */
-  TRAP_PUTSP = 0x24, /* output a byte string */
-  TRAP_HALT = 0x25   /* halt the program */
-};
-
-// Allows to poll the keyboard state and avoid blocking the execution of the
-// program,
-enum {
-  MR_KBSR = 0xFE00, /* Keyboard Status */
-  MR_KBDR = 0xFE02, /* Keyboard data */
-};
 
 void update_flags(uint16_t r) {
   if (reg[r] == 0) {
