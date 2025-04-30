eb97c722efd91b35e84bc095077c00f8a9aa16a4
Rishit <rishitbansals@gmail.com>
2025-04-30 02:40:46 -0400

    adapted memread and write for audio

--- src/memory.c
+++ src/memory.c
@@ -12,20 +12,24 @@ int (*get_char_func)(void) = getchar;
 
 void mem_write(uint16_t address, uint16_t value) {
   if (address == MR_AUDIO_DATA) {
+    if (reg[R_R0] == 0x5000) {
+      audio_reset_buffering();
+    }
     audio_output(value);
+  } else {
+    memory[address] = value;
   }
-  memory[address] = value;
 }
 
 uint16_t mem_read(uint16_t address) {
-  if (address == MR_KBSR) {
-    if (check_key_func) {
-      memory[MR_KBSR] = (1 << 15);
-      memory[MR_KBDR] = (uint16_t)get_char_func();
-    } else {
-      memory[MR_KBSR] = 0;
-    }
-  }
+  // if (address == MR_KBSR) {
+  //   if (check_key_func) {
+  //     memory[MR_KBSR] = (1 << 15);
+  //     memory[MR_KBDR] = (uint16_t)get_char_func();
+  //   } else {
+  //     memory[MR_KBSR] = 0;
+  //   }
+  // }
 
   return memory[address];
 }
