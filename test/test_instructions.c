573ac4008df8a69517f2110cfd538807b72395f3
Rixhee <rishitbansals@gmail.com>
2025-05-03 19:52:19 +0000

    updated unit tests for instructions and memory

--- test/test_instructions.c
+++ test/test_instructions.c
@@ -6,95 +6,178 @@
 #include "../src/memory.h"
 #include "../src/utils.h"
 
-// Test sign extension for a positive number with no extension needed
+// NOLINTBEGIN
+
+// --- sign_extend tests ---
+
 Test(sign_extend, positive_number_no_extension) {
   cr_assert(eq(u16, sign_extend(0b01111, 5), 0b01111));
 }
 
-// Test sign extension for a negative number with extension
 Test(sign_extend, negative_number_extension) {
   cr_assert(eq(u16, sign_extend(0b10000, 5), 0xFFF0));
 }
 
-// Test ADD instruction in register mode (adds values in two registers)
+// --- ADD instruction ---
+
 Test(add_instr, register_mode_adds_values) {
   reg[R_R1] = 3;
   reg[R_R2] = 4;
-  uint16_t instr = (OP_ADD << 12) | (R_R0 << 9) | (R_R1 << 6) | (0 << 5) | R_R2;
-  add_instr(&instr);
+  uint32_t instr = (OP_ADD << 12) | (R_R0 << 9) | (R_R1 << 6) | (0 << 5) | R_R2;
+  add_instr(instr);
   cr_assert(eq(u16, reg[R_R0], 7));
 }
 
-// Test AND instruction in register mode (performs bitwise AND on two registers)
+Test(add_instr, immediate_mode_adds_value) {
+  reg[R_R1] = 5;
+  uint32_t instr =
+      (OP_ADD << 12) | (R_R0 << 9) | (R_R1 << 6) | (1 << 5) | (0x1F);
+  add_instr(instr);
+  cr_assert(eq(u16, reg[R_R0], 5 + sign_extend(0x1F, 5)));
+}
+
+// --- AND instruction ---
+
 Test(and_instr, register_mode_and) {
   reg[R_R1] = 0xAAAA;
   reg[R_R2] = 0x0F0F;
-  uint16_t instr = (OP_AND << 12) | (R_R0 << 9) | (R_R1 << 6) | (0 << 5) | R_R2;
-  and_instr(&instr);
+  uint32_t instr = (OP_AND << 12) | (R_R0 << 9) | (R_R1 << 6) | (0 << 5) | R_R2;
+  and_instr(instr);
   cr_assert(eq(u16, reg[R_R0], 0x0A0A));
 }
 
-// Test NOT instruction (inverts the bits of a register)
+Test(and_instr, immediate_mode_and) {
+  reg[R_R1] = 0x0F0F;
+  uint32_t instr = (OP_AND << 12) | (R_R0 << 9) | (R_R1 << 6) | (1 << 5) | 0x0F;
+  and_instr(instr);
+  cr_assert(eq(u16, reg[R_R0], 0x0F0F & sign_extend(0x0F, 5)));
+}
+
+// --- NOT instruction ---
+
 Test(not_instr, inverts_bits) {
   reg[R_R1] = 0xAAAA;
-  uint16_t instr = (OP_NOT << 12) | (R_R0 << 9) | (R_R1 << 6) | 0x3F;
-  not_instr(&instr);
+  uint32_t instr = (OP_NOT << 12) | (R_R0 << 9) | (R_R1 << 6) | 0x3F;
+  not_instr(instr);
   cr_assert(eq(u16, reg[R_R0], (uint16_t)(~0xAAAA)));
 }
 
-// Test BR instruction (branches if the condition is true)
+// --- BRANCH instruction ---
+
 Test(branch_instr, condition_true_branches) {
   reg[R_PC] = 0x3000;
   reg[R_COND] = FL_POS;
-  uint16_t offset = 0x1F;  // 5-bit offset
-  uint16_t instr = (OP_BR << 12) | (0x1 << 9) | offset;
-  branch_instr(&instr);
+  uint16_t offset = 0x1F;  // 9-bit offset
+  uint32_t instr = (OP_BR << 12) | (FL_POS << 9) | offset;
+  branch_instr(instr);
   cr_assert(eq(u16, reg[R_PC], 0x3000 + sign_extend(offset, 9)));
 }
 
-// Test JMP instruction (sets the PC to the value in a register)
+Test(branch_instr, condition_false_no_branch) {
+  reg[R_PC] = 0x3000;
+  reg[R_COND] = FL_NEG;
+  uint16_t offset = 0x1F;
+  uint32_t instr = (OP_BR << 12) | (FL_POS << 9) | offset;
+  branch_instr(instr);
+  cr_assert(eq(u16, reg[R_PC], 0x3000));  // PC should not change
+}
+
+// --- JMP instruction ---
+
 Test(jump_instr, sets_pc_to_register) {
   reg[R_R1] = 0xBEEF;
-  uint16_t instr = (OP_JMP << 12) | (R_R1 << 6);
-  jump_instr(&instr);
+  uint32_t instr = (OP_JMP << 12) | (R_R1 << 6);
+  jump_instr(instr);
   cr_assert(eq(u16, reg[R_PC], 0xBEEF));
 }
 
-// Test JSR instruction (sets the PC to a register value with an offset)
+// --- JSR/JSRR instruction ---
+
 Test(jump_register_instr, jsr_offset_mode) {
   reg[R_PC] = 0x3000;
   uint16_t offset = 0x7FF;
-  uint16_t instr = (OP_JSR << 12) | (1 << 11) | offset;  // JSR offset mode
-  jump_register_instr(&instr);
+  uint32_t instr = (OP_JSR << 12) | (1 << 11) | offset;  // JSR
+  jump_register_instr(instr);
   cr_assert(eq(u16, reg[R_PC], 0x3000 + sign_extend(offset, 11)));
 }
 
-// Test LDI instruction (loads a value from memory at an address specified by
-// another memory location)
+Test(jump_register_instr, jsrr_register_mode) {
+  reg[R_PC] = 0x4000;
+  reg[R_R2] = 0xBEEF;
+  uint32_t instr = (OP_JSR << 12) | (R_R2 << 6);  // JSRR
+  jump_register_instr(instr);
+  cr_assert(eq(u16, reg[R_PC], 0xBEEF));
+}
+
+// --- LDI instruction ---
+
 Test(ldi_instr, loads_indirect_memory_value) {
   reg[R_PC] = 0x3000;
   memory[0x3002] = 0x1234;
   memory[0x1234] = 0xBEEF;
-  uint16_t instr = (OP_LDI << 12) | (R_R0 << 9) | (0x2);
-  ldi_instr(&instr);
+  uint32_t instr = (OP_LDI << 12) | (R_R0 << 9) | 0x2;
+  ldi_instr(instr);
   cr_assert(eq(u16, reg[R_R0], 0xBEEF));
 }
 
-// Test LD instruction (loads a value from memory relative to the PC)
+// --- LD instruction ---
+
 Test(load_instr, loads_pc_relative_value) {
   reg[R_PC] = 0x3000;
   memory[0x3005] = 0xABCD;
-  uint16_t instr = (OP_LD << 12) | (R_R0 << 9) | (0x5);
-  load_instr(&instr);
+  uint32_t instr = (OP_LD << 12) | (R_R0 << 9) | 0x5;
+  load_instr(instr);
   cr_assert(eq(u16, reg[R_R0], 0xABCD));
 }
 
-// Test ST instruction (stores a value from a register into memory relative to
-// the PC)
+// --- LDR instruction ---
+
+Test(load_reg_instr, loads_base_reg_plus_offset) {
+  reg[R_R1] = 0x4000;
+  memory[0x4005] = 0xCAFE;
+  uint32_t instr = (OP_LDR << 12) | (R_R0 << 9) | (R_R1 << 6) | 0x5;
+  load_reg_instr(instr);
+  cr_assert(eq(u16, reg[R_R0], 0xCAFE));
+}
+
+// --- LEA instruction ---
+
+Test(load_eff_addr_instr, loads_pc_plus_offset) {
+  reg[R_PC] = 0x3000;
+  uint32_t instr = (OP_LEA << 12) | (R_R0 << 9) | 0x10;
+  load_eff_addr_instr(instr);
+  cr_assert(eq(u16, reg[R_R0], 0x3000 + sign_extend(0x10, 9)));
+}
+
+// --- ST instruction ---
+
 Test(store_instr, stores_value_pc_relative) {
   reg[R_PC] = 0x3000;
   reg[R_R1] = 0xBEEF;
-  uint16_t instr = (OP_ST << 12) | (R_R1 << 9) | (0x1);
-  store_instr(&instr);
+  uint32_t instr = (OP_ST << 12) | (R_R1 << 9) | 0x1;
+  store_instr(instr);
   cr_assert(eq(u16, memory[0x3001], 0xBEEF));
 }
+
+// --- STR instruction ---
+
+Test(store_reg_instr, stores_value_base_reg_plus_offset) {
+  reg[R_R1] = 0x4000;
+  reg[R_R2] = 0xBEEF;
+  uint32_t instr = (OP_STR << 12) | (R_R2 << 9) | (R_R1 << 6) | 0x5;
+  store_reg_instr(instr);
+  cr_assert(eq(u16, memory[0x4005], 0xBEEF));
+}
+
+// --- STI instruction ---
+
+Test(store_indirect_instr, stores_indirect_value) {
+  reg[R_PC] = 0x3000;
+  reg[R_R1] = 0xDEAD;
+  memory[0x3002] = 0x4321;
+  uint32_t instr = (OP_STI << 12) | (R_R1 << 9) | 0x2;
+  store_indirect_instr(instr);
+  cr_assert(eq(u16, memory[0x4321], 0xDEAD));
+}
+
+// NOLINTEND
--- test/test_memory.c
+++ test/test_memory.c
@@ -7,13 +7,9 @@
 #include "../src/memory.h"
 #include "../src/utils.h"
 
-uint16_t key_pressed = 0;
+// NOLINTBEGIN
 
-// Mock check_key function
-uint16_t fake_check_key(void) { return key_pressed; }
-int fake_get_char(void) {
-  return 0x41;  // ASCII for 'A'
-}
+// --- Basic mem_write to normal memory ---
 
 Test(mem_write, writes_to_memory) {
   memset(memory, 0, sizeof(memory));
@@ -21,8 +17,12 @@ Test(mem_write, writes_to_memory) {
   uint16_t value = 0x1234;
 
   mem_write(address, value);
-  cr_assert(eq(u16, memory[address], value));
+
+  cr_assert(eq(u16, memory[address], value),
+            "Memory write failed at normal address");
 }
+
+// --- Basic mem_read from normal memory ---
 
 Test(mem_read, normal_memory_read) {
   memset(memory, 0, sizeof(memory));
@@ -32,17 +32,22 @@ Test(mem_read, normal_memory_read) {
   mem_write(address, value);
   uint16_t result = mem_read(address);
 
-  cr_assert(eq(u16, result, value));
+  cr_assert(eq(u16, result, value), "Memory read returned wrong value");
 }
 
-Test(mem_read, keyboard_status_register_key_pressed) {
+// --- Special case: write to MR_AUDIO_DATA ---
+
+Test(mem_write, writes_to_audio_memory_triggers_audio_output) {
+  // Setup: MR_AUDIO_DATA is a special address for audio
   memset(memory, 0, sizeof(memory));
-  key_pressed = 1;
-  check_key_func = fake_check_key;
-  get_char_func = fake_get_char;
 
-  uint16_t result = mem_read(MR_KBSR);
+  reg[R_R0] = AUDIO_ADDRESS;
 
-  cr_assert(eq(u16, result, (1 << 15)));
-  cr_assert(eq(u16, memory[MR_KBDR], 0x41));  // ASCII for 'A'
+  uint16_t value = 0xABCD;
+  mem_write(MR_AUDIO_DATA, value);
+
+  cr_assert(eq(u16, memory[MR_AUDIO_DATA], 0),
+            "Memory at MR_AUDIO_DATA should not be directly modified");
 }
+
+// NOLINTEND
