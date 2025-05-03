2baea92bb9091629021a104a34a5f7b6aecd7096
Rishit <rishitbansals@gmail.com>
2025-05-03 11:34:43 -0400

    added mp3 compat

--- src/utils.c
+++ src/utils.c
@@ -70,9 +70,10 @@ void read_image_file(FILE* file) {
 
 int read_image(const char* image_path) {
   const char* output_obj = "audio.obj";
-  char* suffix = ".wav";
+  char* suffix_wav = ".wav";
+  char* suffix_mp3 = ".mp3";
 
-  if (!strcmp(image_path + strlen(image_path) - strlen(suffix), suffix)) {
+  if (!strcmp(image_path + strlen(image_path) - strlen(suffix_wav), suffix_wav) || !strcmp(image_path + strlen(image_path) - strlen(suffix_mp3), suffix_mp3)) {
     const char* output_pcm = "audio.pcm";
     if (process_audio(image_path, output_pcm) == 0) {
       pcm_to_obj(output_pcm, output_obj);
