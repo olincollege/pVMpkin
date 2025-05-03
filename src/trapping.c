aad102d1c330410ef3f7df67aa4d6f45ae031ea4
eddydpan <eddydotpan@gmail.com>
2025-05-03 13:25:12 -0400

    Change perror to utils error_and_exit

--- src/trapping.c
+++ src/trapping.c
@@ -2,63 +2,67 @@
 #include "memory.h"
 
 void trap_getc(void) {
-  reg[R_R0] = (uint16_t)getc(stdin);
-  if (reg[R_R0] == EOF) {
-    perror("Failed to get character in GETC.");
+  int input = getc(stdin);
+  if (input == EOF) {
+    error_and_exit("Failed to get character in GETC.");
+  } 
+  
+  else {
+    reg[R_R0] = (uint16_t)input;
+    update_flags(R_R0);
   }
-  update_flags(R_R0);
 }
 void trap_out(void) {
   if (putc((char)reg[R_R0], stdout) == EOF) {
-    perror("Failed to print in OUT");
+    error_and_exit("Failed to print in OUT");
   }
-  if (fflush(stdout) == EOF) perror("Failed to flush in OUT.");
+  if (fflush(stdout) == EOF) error_and_exit("Failed to flush in OUT.");
 }
-void trap_puts(void) {                   // double check that * instead of [] works
-  uint16_t* c = memory + reg[R_R0];  // get char pointer
-  while (*c) {
-    if (putc((char)*c, stdout) == EOF) {
-      perror("Failed to put in PUTS.");
+void trap_puts(void) {
+  uint16_t* chr = memory + reg[R_R0];  // get char pointer
+  while (*chr) {
+    if (putc((char)*chr, stdout) == EOF) {
+      error_and_exit("Failed to put in PUTS.");
     }
-    ++c;
+    ++chr;
   }
-  if (fflush(stdout) == EOF) perror("Failed to flush in PUTS.");
+  if (fflush(stdout) == EOF) error_and_exit("Failed to flush in PUTS.");
 }
 
 void trap_in(void) {
   printf("Enter a character: ");
-  char c = getc(stdin);
-  if ((uint16_t)c == EOF) {
-    perror("Failed to get input character in IN");
+  char chr = getc(stdin);
+  if ((uint16_t)chr == EOF) {
+    error_and_exit("Failed to get input character in IN");
   }
-  if (putc(c, stdout) == EOF) {
-    perror("Failed to print input character in IN.");
+  if (putc(chr, stdout) == EOF) {
+    error_and_exit("Failed to print input character in IN.");
   }
   if (fflush(stdout) == EOF) {
-    perror("Failed to flush stdout in IN.");
+    error_and_exit("Failed to flush stdout in IN.");
   }
-  reg[R_R0] = (uint16_t)c;
+  reg[R_R0] = (uint16_t)chr;
   update_flags(R_R0);
 }
 void trap_putsp(void) {                  // double check that * instead of [] works
-  uint16_t* c = memory + reg[R_R0];  // get char pointer
-  while (*c) {
-    char c1 = (*c) & 0xFF;  // 8 bits ; mask
-    if (putc(c1, stdout) == EOF) {
-      perror("Failed to print 1st char in PUTSP.");
+  uint16_t* chr = memory + reg[R_R0];  // get char pointer
+  while (*chr) {
+    char chr1 = (*chr) & 0xFF;  // 8 bits ; mask
+    if (putc(chr1, stdout) == EOF) {
+      error_and_exit("Failed to print 1st char in PUTSP.");
     }
-    char c2 = (*c) >> 8;  // 8 bits ; right shift
-    if (c2) {             // if c2 exists
-      if (putc(c2, stdout) == EOF) {
-        perror("Failed to print 2nd char in PUTSP.");
+    char chr2 = (*chr) >> 8;  // 8 bits ; right shift
+    if (chr2) {             // if c2 exists
+      if (putc(chr2, stdout) == EOF) {
+        error_and_exit("Failed to print 2nd char in PUTSP.");
       }
     }
-    ++c;
+    ++chr;
   }
-  if (fflush(stdout) == EOF) perror("Failed to flush stdout in PUTSP.");
+  if (fflush(stdout) == EOF) error_and_exit("Failed to flush stdout in PUTSP.");
 }
 void trap_halt(int* running) {
   printf("HALT");
-  if (fflush(stdout) == EOF) perror("Failed to flush stdout in HALT.");
+  if (fflush(stdout) == EOF) error_and_exit("Failed to flush stdout in HALT.");
   *running = 0;
 }
