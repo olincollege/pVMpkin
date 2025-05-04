2dd8441262e1bece8812203d687e8dd3e714975a
Rixhee <rishitbansals@gmail.com>
2025-05-04 05:20:54 -0400

    removed buffer reset func from mem write

--- src/memory.c
+++ src/memory.c
@@ -10,9 +10,6 @@ uint16_t memory[MEMORY_MAX];
 
 void mem_write(uint16_t address, uint16_t value) {
   if (address == MR_AUDIO_DATA) {
-    if (reg[R_R0] == AUDIO_ADDRESS) {
-      audio_reset_buffering();
-    }
     audio_output(value);
   } else {
     memory[address] = value;
